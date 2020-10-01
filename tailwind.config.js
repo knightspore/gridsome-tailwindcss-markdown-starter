a5e03b42472cb78b7d759a5268ae1e6f6c424835
knightspore <cslemon96@gmail.com>
2020-10-01 18:02:57 +0200

    Added purge settings to Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './src/**/*.vue',
+      './src/**/*.js',
+      './src/**/*.jsx',
+      './src/**/*.pug',
+      './src/**/*.md'
+    ],
+  },
   theme: {
     extend: {},
   },
