b9fdc6ddece847a875c3b0bd7407fa97fec935f1
knightspore <knightspore@LAPTOP-RMT7FT4Q.localdomain>
2020-10-02 19:07:25 +0200

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
   purge: {
     enabled: process.env.NODE_ENV === 'production',
     content: [
@@ -13,9 +9,4 @@ module.exports = {
       './src/**/*.md'
     ],
   },
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
 }
